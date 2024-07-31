4ae74d367f94edc21fce09d58c26cbc3741415d6
jaehwan02 <jhrick0129@gmail.com>
2024-08-01 02:33:12 +0900

    sidebar hard to map

--- src/renderer/src/components/Sidebar.tsx
+++ src/renderer/src/components/Sidebar.tsx
@@ -1,35 +1,15 @@
-/*
-We're constantly improving the code you see. 
-Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
-*/
-
-// import PropTypes from "prop-types";
-// import React from "react";
 import { SidebarItem } from "./SidebarItem";
 import sidebarItemIcon from "@assets/img/icon-16.svg";
+import { Link } from "react-router-dom";
 
-// interface Props {
-//   sidebarItemIcon: string;
-//   sidebarItemImg: string;
-//   sidebarItemIcon1: string;
-//   sidebarItemIcon2: string;
-//   sidebarItemIcon3: string;
-//   sidebarItemIcon4: string;
-//   sidebarItemIcon5: string;
-//   sidebarItemIcon6: string;
-// }
+export const Sidebar = (): JSX.Element => {
+  const tab = [
+    { name: "홈", link: "/", icon: sidebarItemIcon },
+    { name: "탐색", link: "/explore", icon: sidebarItemIcon },
+    { name: "본문", link: "/article", icon: sidebarItemIcon },
+    { name: "단어", link: "/word", icon: sidebarItemIcon },
+  ];
 
-export const Sidebar = () //   {
-//   sidebarItemIcon = "/img/icon-4.svg",
-//   sidebarItemImg = "/img/icon-4.svg",
-//   sidebarItemIcon1 = "/img/icon-4.svg",
-//   sidebarItemIcon2 = "/img/icon-4.svg",
-//   sidebarItemIcon3 = "/img/icon-4.svg",
-//   sidebarItemIcon4 = "/img/icon-5.svg",
-//   sidebarItemIcon5 = "/img/icon-6.svg",
-//   sidebarItemIcon6 = "/img/icon-7.svg",
-// }: Props
-: JSX.Element => {
   return (
     <div
       className={`w-[200px] h-[100dvh] bg-[#00000000] rounded-[8.5px_0px_0px_8.5px] flex flex-col justify-between`}
@@ -40,6 +20,11 @@ export const Sidebar = () //   {
             기본
           </div>
           <div className="flex flex-col items-start self-stretch w-full flex-[0_0_auto]">
+            {tab.map((a) => (
+              <Link to={a.link}>
+                <SidebarItem darkMode icon={a.icon} text={a.name} />
+              </Link>
+            ))}
             <SidebarItem darkMode icon={sidebarItemIcon} selected text="홈" />
             <SidebarItem darkMode icon={sidebarItemIcon} text="탐색" />
             <SidebarItem darkMode icon={sidebarItemIcon} text="추천" />
@@ -68,14 +53,3 @@ export const Sidebar = () //   {
     </div>
   );
 };
-
-// Sidebar.propTypes = {
-//   sidebarItemIcon: PropTypes.string,
-//   sidebarItemImg: PropTypes.string,
-//   sidebarItemIcon1: PropTypes.string,
-//   sidebarItemIcon2: PropTypes.string,
-//   sidebarItemIcon3: PropTypes.string,
-//   sidebarItemIcon4: PropTypes.string,
-//   sidebarItemIcon5: PropTypes.string,
-//   sidebarItemIcon6: PropTypes.string,
-// };
