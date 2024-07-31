80529401c7f9d75995f037c53d2332aeadef93e4
obtuse-triangle <me@obtuse.kr>
2024-08-01 04:04:01 +0900

    router update

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,12 +3,12 @@ import "./assets/main.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+// import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <HashRouter>
+    {/* <HashRouter future={{ v7_startTransition: true }}> */}
       <App />
-    </HashRouter>
+    {/* </HashRouter> */}
   </React.StrictMode>,
 );
--- src/renderer/src/pages/Article/Article.tsx
+++ src/renderer/src/pages/Article/Article.tsx
@@ -1,3 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Article(): JSX.Element {
-  return <div></div>;
+  return (
+    <div>
+      <Link to="./ocr" relative="path">
+        <div className="flex items-center justify-center w-[200px] h-[200px] bg-red-500">
+          <h1>OCR</h1>
+        </div>
+      </Link>
+    </div>
+  );
 }
