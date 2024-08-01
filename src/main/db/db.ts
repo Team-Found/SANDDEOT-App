01bfde2111ac38bbb03ec3f73065b27d3f1fcfe2
jaehwan02 <jhrick0129@gmail.com>
2024-08-02 05:09:49 +0900

    db move

--- src/main/db/db.ts
+++ src/main/db/db.ts
@@ -2,7 +2,7 @@ import * as sqlite3 from "sqlite3";
 const path = require("path");
 
 const db = new sqlite3.Database(
-  path.resolve(__dirname, "../../../SANDDOET.db"),
+  path.resolve(__dirname, "./SANDDOET.db"),
   sqlite3.OPEN_READWRITE,
   (err) => {
     if (err) {
