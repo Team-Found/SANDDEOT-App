a7c9341da2ecde55d2157bb78be374bc40a434e0
jaehwan02 <jhrick0129@gmail.com>
2024-08-02 04:03:19 +0900

    add updateTextFn

--- src/main/db.ts
+++ src/main/db.ts
@@ -328,6 +328,51 @@ const newText = (
   });
 };
 
-newText("JJJJunho", new Date(), "junho is mmotsanggim", "", 3, 1).then((a) => {
+// newText("JJJJunho", new Date(), "junho is mmotsanggim", "", 3, 1).then((a) => {
+//   console.log(a);
+// });
+
+const updateText = (
+  bodyID: number,
+  option: {
+    title?: string;
+    body?: string;
+    image?: Blob;
+    translated?: string;
+    feedback?: string;
+    score?: number;
+    editDate?: Date;
+    IMPP?: string;
+    level?: number;
+  },
+): Promise<number> => {
+  const sql = `UPDATE Body SET ${Object.keys(option).map((key) =>
+    key ? `${key} = ?` : "",
+  )} WHERE bodyID = ?`;
+
+  // const sql = `UPDATE Body SET bodyID = ?, title = `;
+  return new Promise((resolve, reject) => {
+    db.run(
+      sql,
+      [
+        ...Object.values(option).filter(
+          (item) => item !== undefined && item !== null,
+        ),
+        bodyID,
+      ],
+      (err) => {
+        if (err) {
+          console.error("SQL error:", err.message);
+          reject();
+        } else {
+          console.log(`success`);
+          resolve(0);
+        }
+      },
+    );
+  });
+};
+
+updateText(1, { title: "jaehwan" }).then((a) => {
   console.log(a);
 });
