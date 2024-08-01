5220dfb39a56ef15ed3c6ba55c2aea00933b060b
obtuse-triangle <me@obtuse.kr>
2024-08-02 03:10:46 +0900

    update LearnAnalytics Function

--- src/main/db.ts
+++ src/main/db.ts
@@ -208,7 +208,24 @@ function dateToEpoch(thedate): Date {
   return thedate.setHours(0, 0, 0, 0);
 }
 
-const LearnAnalytics = (
+const LearnEdit = (learnID?: number): Promise<LeanEdit[]> => {
+  const sql = `SELECT * FROM LearnEdit ${learnID ? `WHERE learnID = ?` : ""}`;
+  return new Promise((resolve, reject) => {
+    db.all(sql, [learnID], (err, rows: LeanEdit[]) => {
+      if (err) {
+        console.error("SQL error:", err.message);
+        reject();
+      } else if (rows) {
+        resolve(rows);
+      } else {
+        console.log(`No word found with ??}`);
+        reject();
+      }
+    });
+  });
+};
+
+const LearnAnalytics = async (
   anStartDate: Date = new Date(0),
   anEndDate: Date = new Date(),
 ): Promise<LearnAnalytics[]> => {
@@ -216,69 +233,48 @@ const LearnAnalytics = (
 
   return new Promise((resolve, reject) => {
     const result = [] as LearnAnalytics[];
-    db.each(
+    db.all(
       sql,
       [
         Math.floor(anStartDate.getTime()) / 1000,
         Math.floor(anEndDate.getTime()) / 1000,
       ],
-      (err, row: Learn) => {
+      (err, rows: Learn[]) => {
         if (err) {
-          console.error("SQL error:", err.message);
+          // console.error("SQL error:", err.message);
           reject();
-        } else if (row) {
-          const learnDate = new Date(row.startDate * 1000);
-          let status = false;
-          result.forEach((learn) => {
-            if (learn.date === dateToEpoch(learnDate)) {
-              learn.learnID.push(row.learnID);
-              learn.duration += row.endDate - row.startDate;
-              status = true;
-              db.all(
-                "SELECT * FROM LearnEdit WHERE learnID = ?",
-                [row.learnID],
-                (err, rows: LeanEdit[]) => {
-                  if (err) {
-                    console.error("SQL error:", err.message);
-                    reject();
-                  }
-                  learn.edit.push(...rows);
-                },
-              );
+        } else if (rows) {
+          console.log(rows);
+          const promises = rows.map(async (row) => {
+            const learnDate = new Date(row.startDate * 1000);
+            let status = false;
+            const pormises2 = result.map(async (learn) => {
+              if (learn.date === dateToEpoch(learnDate)) {
+                learn.learnID.push(row.learnID);
+                learn.duration += row.endDate - row.startDate;
+                status = true;
+                const edit = await LearnEdit(row.learnID);
+                learn.edit.push(...edit);
+              }
+            });
+            await Promise.all(pormises2);
+            if (!status) {
+              const edit = await LearnEdit(row.learnID);
+              result.push({
+                date: dateToEpoch(learnDate),
+                learnID: [row.learnID],
+                duration: row.endDate - row.startDate,
+                edit: edit,
+              });
             }
+            return row;
           });
-          if (!status) {
-            db.all(
-              "SELECT * FROM LearnEdit WHERE learnID = ?",
-              [row.learnID],
-              (err, rows: LeanEdit[]) => {
-                if (err) {
-                  console.error("SQL error:", err.message);
-                  reject();
-                }
-                result.push({
-                  date: dateToEpoch(learnDate),
-                  learnID: [row.learnID],
-                  duration: row.endDate - row.startDate,
-                  edit: rows,
-                });
-              },
-            );
-          }
+          Promise.all(promises).then(() => {
+            resolve(result);
+          });
         } else {
           console.log(`No word found with ??}`);
           reject();
-        }
-      },
-      (err, n) => {
-        if (err) {
-          console.error("SQL error:", err.message);
-          reject();
-        } else {
-          console.log(`Returned ${n} rows`);
-          setTimeout(() => {
-            resolve(result);
-          }, 2000);
         }
       },
     );
