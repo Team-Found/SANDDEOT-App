ed14b3558dc898f06ce83fe378e315366bf31af7
obtuse-triangle <me@obtuse.kr>
2024-08-03 04:10:47 +0900

    shadcn/ui와 호환시키기

--- src/renderer/src/routes/root.tsx
+++ src/renderer/src/routes/root.tsx
@@ -5,7 +5,7 @@ export default function Root(): JSX.Element {
   return (
     <div className="flex items-center justify-between flex-1 grow overflow-hidden self-stretch">
       <Sidebar />
-      <div className="flex flex-col items-start gap-[50px] px-[30px] py-[42px] flex-1 self-stretch grow bg-variable-collection-primarybg overflow-y-scroll h-[100dvh]">
+      <div className="flex flex-col bg-background text-foreground items-start gap-[50px] px-[30px] py-[42px] flex-1 self-stretch grow bg-variable-collection-primarybg overflow-y-scroll h-[100dvh]">
         <Outlet />
       </div>
     </div>
