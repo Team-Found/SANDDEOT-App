c5201260825ffacc8b9d91fd626abccb04f75e2d
obtuse-triangle <me@obtuse.kr>
2024-08-04 23:35:20 +0900

    add rss func

--- src/main/utils/rss/rss.ts
+++ src/main/utils/rss/rss.ts
@@ -21,3 +21,34 @@ export async function getRssFeed(url: string): Promise<Feed> {
   const feed = await parser.parseURL(url);
   return feed;
 }
+
+export async function getRssFeeds(urls: string[]): Promise<Feed[]> {
+  const feeds = await Promise.all(urls.map((url) => getRssFeed(url)));
+  return feeds;
+}
+
+export async function getRssFeedsItems(urls: string[]): Promise<Parser.Item[]> {
+  const feeds = await getRssFeeds(urls);
+
+  //order by time
+  const items = feeds.flatMap((feed) => feed.items);
+  items.sort((a, b) => {
+    return (
+      new Date(b?.isoDate as string)?.getTime() -
+      new Date(a?.isoDate as string)?.getTime()
+    );
+  });
+
+  return items;
+}
+
+getRssFeedsItems(["https://www.reddit.com/.rss"]).then((items) => {
+  console.log(items);
+});
+
+// getRssFeed("https://www.reddit.com/.rss").then((feed) => {
+//   console.log(feed.title);
+//   feed.items.forEach((item) => {
+//     console.log(item.title + ":" + item.link);
+//   });
+// });
