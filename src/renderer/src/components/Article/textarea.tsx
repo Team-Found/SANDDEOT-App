de05624058784ba7ae2d61964704b7db17a586ea
gamzamandu <galbimandu1126@gmail.com>
2024-08-04 18:37:50 +0900

    bugFix : 부모요소의 굵기에 영향받는 것을 수정

--- src/renderer/src/components/Article/textarea.tsx
+++ src/renderer/src/components/Article/textarea.tsx
@@ -12,16 +12,15 @@ const Textarea: React.FC<TextareaProps> = ({ value, onChange, onSubmit }) => {
   useEffect(() => {
     const textarea = textareaRef.current;
     if (textarea) {
-      textarea.style.height = 'auto'; // 높이를 초기화하여 scrollHeight를 정확히 계산할 수 있도록 함
-      textarea.style.height = `${textarea.scrollHeight}px`; // 콘텐츠의 높이에 맞게 조정
+      textarea.style.minHeight = 'auto'; // 높이를 초기화하여 scrollHeight를 정확히 계산할 수 있도록 함
+      textarea.style.minHeight = `${textarea.scrollHeight}px`; // 콘텐츠의 높이에 맞게 조정
     }
   }, [value]); // 값이 변경될 때마다 높이를 조정
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (event.key === 'Enter' && !event.shiftKey) {
       event.preventDefault(); // 기본 엔터 동작을 방지
-      onSubmit()
-      alert(`전송된 내용: ${text}`);
+      onSubmit();
     }
   };
 
@@ -31,7 +30,8 @@ const Textarea: React.FC<TextareaProps> = ({ value, onChange, onSubmit }) => {
       value={value}
       onChange={onChange}
       onKeyDown={handleKeyDown} // 키보드 이벤트 핸들러 추가
-      className="w-full p-2 border rounded resize-none overflow-hidden bg-background text-sm px-2 py-2 placeholder:text-muted-foreground focus-visible:ring-1 focus-visible:outline-none focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+      className="w-full p-2 border box-border rounded resize-none bg-background text-sm placeholder:text-muted-foreground font-normal
+      focus-visible:ring-1 focus-visible:outline-none focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
     />
   );
 };
