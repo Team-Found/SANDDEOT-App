b487dd695130b1b745ae92ce5592f10ae5712a4b
jaehwan02 <jhrick0129@gmail.com>
2024-08-04 09:16:48 +0900

    RSSArticle Fn add

--- src/main/utils/db/modules/article/articleAdd.ts
+++ src/main/utils/db/modules/article/articleAdd.ts
@@ -11,6 +11,7 @@ const add = (
   categoryID: number,
   author: string,
   image?: Blob | null,
+  RSSID?: number,
 ): Promise<number> => {
   const sql =
     "INSERT INTO Body (title, date, body, translated, origin, image, categoryID, author) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
@@ -24,14 +25,43 @@ const add = (
       [title, unixTime, body, translated, origin, image, categoryID, author],
       (err) => {
         if (err) {
-          console.error("SQL error:", err.message);
+          console.error("SQL error1:", err.message);
           reject();
-        } else {
+        } else if (RSSID) {
           console.log(`success`);
+          db.get(
+            "SELECT bodyID FROM Body WHERE title = ? and date = ? and body = ? and translated = ? and origin = ? and categoryID = ? and author = ?",
+            [title, unixTime, body, translated, origin, categoryID, author],
+            (err, row: { bodyID: number }) => {
+              if (err) {
+                console.error("SQL error2:", err.message);
+                reject();
+              } else {
+                console.log(`second success`);
+                db.run(
+                  "INSERT INTO RSSArticle (bodyID, RSSID) VALUES (?, ?)",
+                  [row.bodyID, RSSID],
+                  (err, row) => {
+                    if (err) {
+                      console.error("SQL error3:", err.message);
+                      reject();
+                    } else {
+                      console.log(`third success`);
+                      resolve(0);
+                    }
+                  },
+                );
+              }
+            },
+          );
+        } else {
           resolve(0);
         }
       },
     );
   });
 };
+
+add("sd;lkf", new Date(), "hihihi", "", 3, 1, "dlwoghks", null, 1);
+
 export default add;
