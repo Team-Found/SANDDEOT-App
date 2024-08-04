4a585f559e80e6ea9677da13a8070e5c23ca8aae
jaehwan02 <jhrick0129@gmail.com>
2024-08-05 05:03:39 +0900

    폼 화면에서 취소 누르면 이전내용 불러오기

--- src/renderer/src/pages/Article/ArticleAddForm2.tsx
+++ src/renderer/src/pages/Article/ArticleAddForm2.tsx
@@ -62,6 +62,8 @@ export const FormDetail = (): JSX.Element => {
                   textImage,
                 );
               }
+              dispatch(setBody(""));
+              dispatch(setTitle(""));
             }}
           >
             <ButtonDemo />
