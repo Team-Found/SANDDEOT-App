9839bc4e36454716c848892cc0be12b063d46a9b
obtuse-triangle <me@obtuse.kr>
2024-08-14 02:22:23 +0900

    rssAdd 함수 추가

---
+++ src/main/utils/db/modules/rss/rssAdd.ts
@@ -0,0 +1,36 @@
+import db from "../../db";
+import { RSS } from "../../types/Rss";
+
+export default function rssAdd(rss: RSS): Promise<void> {
+  const sql =
+    "INSERT INTO RSS (RSSID, RSSURL, RSSName, RSSImageUrl) VALUES (?, ?, ?, ?)";
+  return new Promise((resolve, reject) => {
+    db.run(
+      sql,
+      [rss.RSSID, rss.RSSURL, rss.RSSName, rss.RSSImageUrl],
+      (err) => {
+        if (err) {
+          console.error("SQL error:", err.message);
+          reject();
+        } else {
+          resolve();
+        }
+      },
+    );
+  });
+}
+
+// test code
+
+// rssAdd({
+//   RSSID: 2,
+//   RSSURL: "https://rss.blog.naver.com/leedongeun.xml",
+//   RSSName: "이동은 블로그",
+//   RSSImageUrl: "https://rss.blog.naver.com/leedongeun.xml",
+// })
+//   .then(() => {
+//     console.log("success");
+//   })
+//   .catch(() => {
+//     console.log("fail");
+//   });
