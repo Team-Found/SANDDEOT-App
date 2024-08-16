a523afde2542c14c94f0fded466774196d6bf70a
obtuse-triangle <me@obtuse.kr>
2024-08-16 12:16:37 +0900

    WinkNLP를 통한 주요문장분석

--- src/renderer/src/components/Chat.tsx
+++ src/renderer/src/components/Chat.tsx
@@ -2,21 +2,27 @@ import summation from "@assets/img/Chat/summation.svg";
 import equipment from "@assets/img/Chat/equipment.svg";
 import question from "@assets/img/Chat/question.svg";
 import send from "@assets/img/Chat/send.svg";
+import React from "react";
 
-export const Chat = (): JSX.Element => {
+interface ChatProps {
+  setCutOff: React.Dispatch<React.SetStateAction<number>>;
+  cutOff: number;
+}
+
+export const Chat: React.FC<ChatProps> = ({ setCutOff, cutOff }) => {
   return (
-    <div className="flex flex-col border-l-[1px]">
-      <div className="w-96 h-full pl-4 pr-3.5 py-5 border-b border-PrimaryBorder flex-col justify-start items-start inline-flex">
-        <div className="self-stretch h-full flex-col justify-center items-start flex">
+    <div className="flex flex-col border-l-[1px] max-w-96 h-full">
+      <div className="w-full pl-4 pr-3.5 py-5 border-b border-PrimaryBorder flex-col justify-start items-start inline-flex">
+        <div className="self-stretch flex-col justify-center items-start flex">
           <div className="justify-start items-center inline-flex">
             <img src={summation} alt="요약" />
-            <div className="text-white text-xl font-semibold font-['Inter'] leading-normal">
+            <div className="text-white text-xl font-semibold leading-normal">
               요약
             </div>
           </div>
         </div>
         <div className="self-stretch pl-1 pr-1.5 py-1.5 rounded shadow-inner justify-center items-center gap-2.5 inline-flex">
-          <div className="grow shrink basis-0 self-stretch text-toolSecondary text-sm font-normal font-['Pretendard Variable'] leading-none">
+          <div className="grow self-stretch text-toolSecondary text-sm font-normal leading-none">
             Borem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
             vulputate libero et velit interdum, ac aliquet odio mattis. Class
             aptent taciti sociosqu ad litora torquent per conubia nostra, per
@@ -24,46 +30,54 @@ export const Chat = (): JSX.Element => {
           </div>
         </div>
       </div>
-      <div className="w-96 h-full pl-4 pr-3.5 py-5 border-b border-PrimaryBorder flex-col justify-start items-start gap-2 inline-flex">
-        <div className="self-stretch h-full flex-col justify-start items-start gap-2 flex">
+      <div className="w-full pl-4 pr-3.5 py-5 border-b border-PrimaryBorder flex-col justify-start items-start gap-2 inline-flex">
+        <div className="self-stretch flex-col justify-start items-start gap-2 flex">
           <div className="justify-start items-start gap-2 inline-flex">
             <img src={equipment} alt="도구" />
-            <div className="text-white text-xl font-semibold font-['Inter'] leading-normal">
+            <div className="text-white text-xl font-semibold leading-normal">
               도구
             </div>
           </div>
-          <div className="w-96 grow shrink basis-0 justify-center items-center inline-flex">
-            <div className="w-80 text-toolSecondary text-sm font-normal font-['Pretendard Variable'] leading-none">
+          <div className="w-full justify-center items-center inline-flex">
+            <div className="w-full text-toolSecondary text-sm font-normal leading-none">
               형태소 하이라이팅
+              <input
+                type="range"
+                min="0"
+                max="0.6"
+                step="0.05"
+                value={cutOff}
+                onChange={(e) => setCutOff(parseFloat(e.target.value))}
+              />
             </div>
           </div>
-          <div className="w-96 grow shrink basis-0 justify-center items-center inline-flex">
-            <div className="w-80 text-toolSecondary text-sm font-normal font-['Pretendard Variable'] leading-none">
+          <div className="w-full shrink justify-center items-center inline-flex">
+            <div className="w-full text-toolSecondary text-sm font-normal leading-none">
               AI마크다운화
             </div>
           </div>
         </div>
       </div>
-      <div className="w-96 h-full pl-4 pr-3.5 py-5 flex-col justify-start items-start gap-2 inline-flex">
-        <div className="self-stretch h-full flex-col justify-start items-start gap-2 flex">
+      <div className="w-full pl-4 pr-3.5 py-5 flex-col justify-start items-start gap-2 inline-flex">
+        <div className="self-stretch flex-col justify-start items-start gap-2 flex">
           <div className="justify-start items-start gap-2 inline-flex">
             <img src={question} alt="도구" />
-            <div className="text-white text-xl font-semibold font-['Inter'] leading-normal">
+            <div className="text-white text-xl font-semibold leading-normal">
               도구
             </div>
           </div>
           <div className="w-full h-8 px-2 py-2 bg-zinc-500 rounded-lg justify-start items-center gap-2.5 inline-flex">
-            <div className="text-stone-950 text-sm font-normal font-['Pretendard Variable'] leading-none">
+            <div className="text-stone-950 text-sm font-normal leading-none">
               남자의 음식을 탐내는 이준호 • 10줄
             </div>
           </div>
-          <div className="w-full max-h-64 h-full overflow-y-auto">
+          <div className="w-full max-h-64 overflow-y-auto">
             <div className="pl-1 justify-start items-start gap-1 inline-flex">
-              <div className="text-toolSecondary text-sm font-extrabold font-['Pretendard Variable'] leading-7">
+              <div className="text-toolSecondary text-sm font-extrabold leading-7">
                 Q.
               </div>
               <div className="grow shrink basis-0 pl-1 pr-1.5 py-1.5 rounded shadow-inner justify-center items-center gap-2.5 flex">
-                <div className="grow shrink basis-0 self-stretch text-toolSecondary text-sm font-normal font-['Pretendard Variable'] leading-none">
+                <div className="grow shrink basis-0 self-stretch text-toolSecondary text-sm font-normal leading-none">
                   Borem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
                   vulputate libero et velit interdum, ac aliquet odio mattis.
                   Class aptent taciti sociosqu ad litora torquent per conubia
@@ -72,11 +86,11 @@ export const Chat = (): JSX.Element => {
               </div>
             </div>
             <div className="pl-1 justify-start items-start gap-1 inline-flex">
-              <div className="text-toolSecondary text-sm font-extrabold font-['Pretendard Variable'] leading-7">
+              <div className="text-toolSecondary text-sm font-extrabold leading-7">
                 A.
               </div>
               <div className="grow shrink basis-0 pl-1 pr-1.5 py-1.5 rounded shadow-inner justify-center items-center gap-2.5 flex">
-                <div className="grow shrink basis-0 self-stretch text-toolSecondary text-sm font-normal font-['Pretendard Variable'] leading-none">
+                <div className="grow shrink basis-0 self-stretch text-toolSecondary text-sm font-normal leading-none">
                   Borem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
                   vulputate libero et velit interdum, ac aliquet odio mattis.
                   Class aptent taciti sociosqu ad litora torquent per conubia
@@ -85,11 +99,11 @@ export const Chat = (): JSX.Element => {
               </div>
             </div>
             <div className="pl-1 justify-start items-start gap-1 inline-flex">
-              <div className="text-toolSecondary text-sm font-extrabold font-['Pretendard Variable'] leading-7">
+              <div className="text-toolSecondary text-sm font-extrabold leading-7">
                 Q.
               </div>
               <div className="grow shrink basis-0 pl-1 pr-1.5 py-1.5 rounded shadow-inner justify-center items-center gap-2.5 flex">
-                <div className="grow shrink basis-0 self-stretch text-toolSecondary text-sm font-normal font-['Pretendard Variable'] leading-none">
+                <div className="grow shrink basis-0 self-stretch text-toolSecondary text-sm font-normal leading-none">
                   Borem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
                   vulputate libero et velit interdum, ac aliquet odio mattis.
                   Class aptent taciti sociosqu ad litora torquent per conubia
@@ -98,11 +112,11 @@ export const Chat = (): JSX.Element => {
               </div>
             </div>
             <div className="pl-1 justify-start items-start gap-1 inline-flex">
-              <div className="text-toolSecondary text-sm font-extrabold font-['Pretendard Variable'] leading-7">
+              <div className="text-toolSecondary text-sm font-extrabold leading-7">
                 A.
               </div>
               <div className="grow shrink basis-0 pl-1 pr-1.5 py-1.5 rounded shadow-inner justify-center items-center gap-2.5 flex">
-                <div className="grow shrink basis-0 self-stretch text-toolSecondary text-sm font-normal font-['Pretendard Variable'] leading-none">
+                <div className="grow shrink basis-0 self-stretch text-toolSecondary text-sm font-normal leading-none">
                   Borem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
                   vulputate libero et velit interdum, ac aliquet odio mattis.
                   Class aptent taciti sociosqu ad litora torquent per conubia
@@ -111,11 +125,11 @@ export const Chat = (): JSX.Element => {
               </div>
             </div>
             <div className="pl-1 justify-start items-start gap-1 inline-flex">
-              <div className="text-toolSecondary text-sm font-extrabold font-['Pretendard Variable'] leading-7">
+              <div className="text-toolSecondary text-sm font-extrabold leading-7">
                 Q.
               </div>
               <div className="grow shrink basis-0 pl-1 pr-1.5 py-1.5 rounded shadow-inner justify-center items-center gap-2.5 flex">
-                <div className="grow shrink basis-0 self-stretch text-toolSecondary text-sm font-normal font-['Pretendard Variable'] leading-none">
+                <div className="grow shrink basis-0 self-stretch text-toolSecondary text-sm font-normal leading-none">
                   Borem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
                   vulputate libero et velit interdum, ac aliquet odio mattis.
                   Class aptent taciti sociosqu ad litora torquent per conubia
@@ -124,11 +138,11 @@ export const Chat = (): JSX.Element => {
               </div>
             </div>
             <div className="pl-1 justify-start items-start gap-1 inline-flex">
-              <div className="text-toolSecondary text-sm font-extrabold font-['Pretendard Variable'] leading-7">
+              <div className="text-toolSecondary text-sm font-extrabold leading-7">
                 A.
               </div>
               <div className="grow shrink basis-0 pl-1 pr-1.5 py-1.5 rounded shadow-inner justify-center items-center gap-2.5 flex">
-                <div className="grow shrink basis-0 self-stretch text-toolSecondary text-sm font-normal font-['Pretendard Variable'] leading-none">
+                <div className="grow shrink basis-0 self-stretch text-toolSecondary text-sm font-normal leading-none">
                   Borem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
                   vulputate libero et velit interdum, ac aliquet odio mattis.
                   Class aptent taciti sociosqu ad litora torquent per conubia
@@ -139,7 +153,7 @@ export const Chat = (): JSX.Element => {
           </div>
           <div className="w-full h-8 p-2 bg-zinc-800 rounded-3xl shadow-inner justify-between items-center inline-flex">
             <div className="grow shrink basis-0 h-3.5 justify-start items-center gap-0.5 flex">
-              <div className="text-zinc-400 text-xs font-normal font-['Pretendard Variable'] leading-none">
+              <div className="text-zinc-400 text-xs font-normal leading-none">
                 궁금한 점을 물어보세요
               </div>
             </div>
--- src/renderer/src/pages/Detail.tsx
+++ src/renderer/src/pages/Detail.tsx
@@ -1,6 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { Chat } from "@renderer/components/Chat";
+import winkNLP from "wink-nlp";
+import model from "wink-eng-lite-web-model";
+import "./mark.css"
 
 export default function Detail(): JSX.Element {
   let { id } = useParams();
@@ -16,23 +19,63 @@ export default function Detail(): JSX.Element {
         console.error("Error fetching article:", error);
       }
     };
-
     fetchArticle();
   }, [id]);
 
+  const resultRef = useRef<HTMLDivElement>(null);
+
+  const nlp = winkNLP( model, ['sbd', 'pos'] )
+  // Acquire "its" and "as" helpers from nlp.
+  const its = nlp.its;
+  const as = nlp.as;
+
+  const text =`Children living in Japan’s hottest city will be given specially designed umbrellas to protect them from the heat, after a summer that saw record-breaking temperatures in many parts of the country.Local authorities in Kumagaya in Saitama prefecture have devised an umbrella that keeps out the rain and doubles as a parasol, the Mainichi Shimbun reported. The umbrellas, which bear the city’s logo and weigh just 336 grams, will be distributed to 9,000 primary schoolchildren next week, the newspaper said. Kumagaya, a city of about 195,000 located 60km north of Tokyo, regularly records the highest temperatures in Japan partly as a result of warm downslope winds created by the Foehn Effect. The city’s government has for the past two years advised younger children to shield themselves from the sun with regular umbrellas on their way to and from school to prevent heatstroke, but some questioned their ability to block out sunlight. Alarmed by a rise in the number of days when the mercury rises to at least 35C, the city decided to hand out the yellow fibreglass umbrellas, including to children who live in Kumagaya but attend schools outside the city, the Mainichi said. The heat-busting brollies will also force children to maintain a reasonable distance from each other, eliminating the need for them to wear masks to prevent the spread of the coronavirus, it added. The measure has come a little late in the day, however. Japan battled its worst heatwave since records began in 1875 in late June, after a premature end to the rainy season. The city of Isesaki, north of Tokyo, registered the country’s highest-ever temperature for that month, at 40.2C, beating the previous June record of 39.8C set in 2011. Tokyo experienced several consecutive days of 35C-plus heat, prompting the government to warn people to save energy or face power cuts, while Kumagaya and five other locations marked highs above 40C on 1 July. Kumagaya’s reputation for furnace-like temperatures was sealed in July 2018, when it battled an all-time high temperature of 41.1C – an unenviable record it shares with the city of Hamamatsu in central Japan. On Friday, the maximum temperature for Kumagaya was a far more comfortable 26C, according to the meteorological agency, although it forecast a rerun to the low 30s next week. Officials had hoped to distribute the umbrellas before the school summer holidays began were delayed by the Covid-19 pandemic. Global heating has prompted Japan’s government to take extra measures and issue a slew of advice on how to prevent heatstroke. Almost all classrooms in public primary and middle schools now have air conditioners, according to the Asahi Shimbun, while the education ministry last year urged teachers to instruct children to wear cool clothing and hats, and to keep hydrated when they travel to and from school. The pandemic has frustrated attempts to keep children cool at school, however, with teachers reporting that many are reluctant to remove their masks, even with encouragement from staff.`;
+  const doc = nlp.readDoc( text );
+  // doc.entities().each((e) => e.markup());
+  const sentenceWiseNormalizedWeights = doc.out(its.sentenceWiseImportance).map(e => e.importance);
+  // const cutoff = 0.9;
+
+  const [cutoff, setCutoff] = useState(0.2);
+  
+  // document.getElementById("result").innerHTML = doc.out(its.markedUpText);
+  // resultRef.current?.appendChild(doc.out(its.markedUpText));
+  
+  useEffect(() => {
+    doc.sentences().each((s: winkNLP.ItemSentence, k) => {
+      const a = (sentenceWiseNormalizedWeights[k]>1-cutoff)? sentenceWiseNormalizedWeights[k]:0;
+      if (a > 0.9) {
+        s.markup("<mark class=\"highlight  hi-1\">", "</mark>");
+      } else if (a > 0.7) {
+        s.markup("<mark class=\"highlight bg-opacity-80 hi-2\">", "</mark>");
+      } else if (a > 0.4) {
+        s.markup("<mark class=\"highlight bg-opacity-60 hi-3\">", "</mark>");
+      } else if (a > 0.1) {
+        s.markup("<mark class=\"highlight bg-opacity-40 hi-4\">", "</mark>");
+      }
+
+    });
+    if (resultRef.current) {
+        resultRef.current.innerHTML = doc.out(its.markedUpText);
+    }
+  }, [doc, its, cutoff]);
+
   return (
-    <div className="flex">
+    <div className="flex w-full max-h-[calc(100dvh-2.5rem)] overflow-hidden flex-1 flex-grow">
+
       {/* <iframe
         src="https://obtuse.kr"
         className="w-3/4 h-3/4 border-2 border-gray-300"
         title="Example Site"
-      /> */}
-      <div className="prose prose-basic dark:prose-invert min-w-full">
+        /> */}
+      <div className="prose prose-basic dark:prose-invert overflow-y-scroll w-full !max-w-full flex-1 flex-grow">
+        <div id="result" ref={resultRef} className="prose prose-basic !max-w-full dark:prose-invert w-full"
+        ></div>
         {article?.body && (
-          <div dangerouslySetInnerHTML={{ __html: article.body }} />
+          <div dangerouslySetInnerHTML={{ __html: article.body }} className="w-full"/>
         )}
       </div>
-      <Chat />
+      
+      <Chat setCutOff={setCutoff} cutOff={cutoff} />
     </div>
   );
 }
