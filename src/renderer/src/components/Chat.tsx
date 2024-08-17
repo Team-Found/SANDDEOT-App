e88b136c70e3dfe3a5c001c7a09f141b5b3c4792
obtuse-triangle <me@obtuse.kr>
2024-08-17 18:12:22 +0900

    WinkNLP 모델 최적화

--- src/renderer/src/components/Chat.tsx
+++ src/renderer/src/components/Chat.tsx
@@ -3,9 +3,9 @@ import equipment from "@assets/img/Chat/equipment.svg";
 import question from "@assets/img/Chat/question.svg";
 import send from "@assets/img/Chat/send.svg";
 import React from "react";
-import { Switch } from "@/components/ui/switch"
+import { Switch } from "@/components/ui/switch";
 import { useState } from "react";
-import { Slider } from "@/components/ui/slider"
+import { Slider } from "@/components/ui/slider";
 
 interface ChatProps {
   setCutOff: React.Dispatch<React.SetStateAction<number>>;
@@ -35,7 +35,7 @@ export const Chat: React.FC<ChatProps> = ({ setCutOff, cutOff }) => {
         </div>
       </div>
       <div className="w-full pl-4 pr-3.5 py-5 border-b border-PrimaryBorder flex-col justify-start items-start gap-2 inline-flex">
-        <div className="self-stretch flex-col justify-start items-start gap-2 flex">
+        <div className="self-stretch flex-col justify-start items-start gap-4 flex">
           <div className="justify-start items-start gap-2 inline-flex">
             <img src={equipment} alt="도구" />
             <div className="text-white text-xl font-semibold leading-normal">
@@ -46,22 +46,27 @@ export const Chat: React.FC<ChatProps> = ({ setCutOff, cutOff }) => {
             <div className="w-full text-toolSecondary text-sm font-normal leading-none">
               <div className="flex justify-between">
                 중요문장 하이라이팅
-                <Switch defaultChecked={sentenceHighlight} onClick={()=>{setSentenceHighlight(!sentenceHighlight)}}/>
+                <Switch
+                  defaultChecked={sentenceHighlight}
+                  onClick={() => {
+                    setSentenceHighlight(!sentenceHighlight);
+                  }}
+                />
               </div>
-              {sentenceHighlight?
-              <Slider defaultValue={[cutOff]} max={0.6} step={0.05} onValueChange={(e) => setCutOff(parseFloat(e))} style={{width: "100%"}} />
-              // <input
-              //   type="range"
-              //   min="0"
-              //   max="0.6"
-              //   step="0.05"
-              //   value={cutOff}
-              //   onChange={(e) => setCutOff(parseFloat(e.target.value))}
-              // />
-              : ():JSX.Element=>{
-                setCutOff(0);
-                return (<></>)
-              }}
+              {sentenceHighlight ? (
+                <Slider
+                  defaultValue={[cutOff]}
+                  max={80}
+                  step={2}
+                  onValueChange={(e) => setCutOff(e)}
+                  style={{ width: "100%", padding: "1rem 0" }}
+                />
+              ) : (
+                ((): React.ReactNode => {
+                  setCutOff(0);
+                  return <></>;
+                })()
+              )}
             </div>
           </div>
           <div className="w-full shrink justify-center items-center inline-flex">
--- src/renderer/src/pages/Detail.tsx
+++ src/renderer/src/pages/Detail.tsx
@@ -1,15 +1,16 @@
-import { useEffect, useState, useRef } from "react";
+import { useMemo, useEffect, useState, useCallback } from "react";
 import { useParams } from "react-router-dom";
 import { Chat } from "@renderer/components/Chat";
 import winkNLP from "wink-nlp";
 import model from "wink-eng-lite-web-model";
-import "./mark.css"
+// import "./mark.css";
 
 export default function Detail(): JSX.Element {
   let { id } = useParams();
   const [article, setArticle] =
     useState<Awaited<ReturnType<typeof window.dbApi.article.detail>>>();
 
+  console.log("Detail");
   useEffect(() => {
     const fetchArticle = async () => {
       try {
@@ -22,56 +23,99 @@ export default function Detail(): JSX.Element {
     fetchArticle();
   }, [id]);
 
-  const resultRef = useRef<HTMLDivElement>(null);
-
-  const nlp = winkNLP( model, ['sbd', 'pos'] )
-  // Acquire "its" and "as" helpers from nlp.
-  const its = nlp.its;
-  const as = nlp.as;
-
-  const text =`Children living in Japan’s hottest city will be given specially designed umbrellas to protect them from the heat, after a summer that saw record-breaking temperatures in many parts of the country.Local authorities in Kumagaya in Saitama prefecture have devised an umbrella that keeps out the rain and doubles as a parasol, the Mainichi Shimbun reported. The umbrellas, which bear the city’s logo and weigh just 336 grams, will be distributed to 9,000 primary schoolchildren next week, the newspaper said. Kumagaya, a city of about 195,000 located 60km north of Tokyo, regularly records the highest temperatures in Japan partly as a result of warm downslope winds created by the Foehn Effect. The city’s government has for the past two years advised younger children to shield themselves from the sun with regular umbrellas on their way to and from school to prevent heatstroke, but some questioned their ability to block out sunlight. Alarmed by a rise in the number of days when the mercury rises to at least 35C, the city decided to hand out the yellow fibreglass umbrellas, including to children who live in Kumagaya but attend schools outside the city, the Mainichi said. The heat-busting brollies will also force children to maintain a reasonable distance from each other, eliminating the need for them to wear masks to prevent the spread of the coronavirus, it added. The measure has come a little late in the day, however. Japan battled its worst heatwave since records began in 1875 in late June, after a premature end to the rainy season. The city of Isesaki, north of Tokyo, registered the country’s highest-ever temperature for that month, at 40.2C, beating the previous June record of 39.8C set in 2011. Tokyo experienced several consecutive days of 35C-plus heat, prompting the government to warn people to save energy or face power cuts, while Kumagaya and five other locations marked highs above 40C on 1 July. Kumagaya’s reputation for furnace-like temperatures was sealed in July 2018, when it battled an all-time high temperature of 41.1C – an unenviable record it shares with the city of Hamamatsu in central Japan. On Friday, the maximum temperature for Kumagaya was a far more comfortable 26C, according to the meteorological agency, although it forecast a rerun to the low 30s next week. Officials had hoped to distribute the umbrellas before the school summer holidays began were delayed by the Covid-19 pandemic. Global heating has prompted Japan’s government to take extra measures and issue a slew of advice on how to prevent heatstroke. Almost all classrooms in public primary and middle schools now have air conditioners, according to the Asahi Shimbun, while the education ministry last year urged teachers to instruct children to wear cool clothing and hats, and to keep hydrated when they travel to and from school. The pandemic has frustrated attempts to keep children cool at school, however, with teachers reporting that many are reluctant to remove their masks, even with encouragement from staff.`;
-  const doc = nlp.readDoc( text );
-  const sentenceWiseNormalizedWeights = doc.out(its.sentenceWiseImportance).map(e => e.importance);
-
-  const [cutoff, setCutoff] = useState(0);
-    
-  useEffect(() => {
-    console.log("updating")
-    doc.sentences().each((s: winkNLP.ItemSentence, k) => {
-      const a = (sentenceWiseNormalizedWeights[k]>1-cutoff)? sentenceWiseNormalizedWeights[k]:0;
-      if (a > 0.9) {
-        s.markup("<mark class=\"highlight hi-1\">", "</mark>");
-      } else if (a > 0.7) {
-        s.markup("<mark class=\"highlight hi-2\">", "</mark>");
-      } else if (a > 0.4) {
-        s.markup("<mark class=\"highlight hi-3\">", "</mark>");
-      } else if (a > 0.1) {
-        s.markup("<mark class=\"highlight hi-4\">", "</mark>");
-      }
-
-    });
-    if (resultRef.current) {
-        resultRef.current.innerHTML = doc.out(its.markedUpText);
+  const quantile = useCallback((arr: number[], q: number) => {
+    const sorted = arr.slice().sort((a, b) => a - b);
+    const pos = (sorted.length - 1) * q;
+    const base = Math.floor(pos);
+    const rest = pos - base;
+    if (sorted[base + 1] !== undefined) {
+      return sorted[base] + rest * (sorted[base + 1] - sorted[base]);
+    } else {
+      return sorted[base];
     }
-  }, [cutoff]);
+  }, []);
+
+  let lastDate = new Date("1970-01-01");
+  let lastData = <></>;
+
+  let nlp = useMemo(() => winkNLP(model, ["sbd", "pos"]), []);
+  let its = useMemo(() => nlp.its, []);
+  let as = useMemo(() => nlp.as, []);
+  let text = useMemo(
+    () =>
+      "Children living in Japan’s hottest city will be given specially designed umbrellas to protect them from the heat, after a summer that saw record-breaking temperatures in many parts of the country.Local authorities in Kumagaya in Saitama prefecture have devised an umbrella that keeps out the rain and doubles as a parasol, the Mainichi Shimbun reported. The umbrellas, which bear the city’s logo and weigh just 336 grams, will be distributed to 9,000 primary schoolchildren next week, the newspaper said. Kumagaya, a city of about 195,000 located 60km north of Tokyo, regularly records the highest temperatures in Japan partly as a result of warm downslope winds created by the Foehn Effect. The city’s government has for the past two years advised younger children to shield themselves from the sun with regular umbrellas on their way to and from school to prevent heatstroke, but some questioned their ability to block out sunlight. Alarmed by a rise in the number of days when the mercury rises to at least 35C, the city decided to hand out the yellow fibreglass umbrellas, including to children who live in Kumagaya but attend schools outside the city, the Mainichi said. The heat-busting brollies will also force children to maintain a reasonable distance from each other, eliminating the need for them to wear masks to prevent the spread of the coronavirus, it added. The measure has come a little late in the day, however. Japan battled its worst heatwave since records began in 1875 in late June, after a premature end to the rainy season. The city of Isesaki, north of Tokyo, registered the country’s highest-ever temperature for that month, at 40.2C, beating the previous June record of 39.8C set in 2011. Tokyo experienced several consecutive days of 35C-plus heat, prompting the government to warn people to save energy or face power cuts, while Kumagaya and five other locations marked highs above 40C on 1 July. Kumagaya’s reputation for furnace-like temperatures was sealed in July 2018, when it battled an all-time high temperature of 41.1C – an unenviable record it shares with the city of Hamamatsu in central Japan. On Friday, the maximum temperature for Kumagaya was a far more comfortable 26C, according to the meteorological agency, although it forecast a rerun to the low 30s next week. Officials had hoped to distribute the umbrellas before the school summer holidays began were delayed by the Covid-19 pandemic. Global heating has prompted Japan’s government to take extra measures and issue a slew of advice on how to prevent heatstroke. Almost all classrooms in public primary and middle schools now have air conditioners, according to the Asahi Shimbun, while the education ministry last year urged teachers to instruct children to wear cool clothing and hats, and to keep hydrated when they travel to and from school. The pandemic has frustrated attempts to keep children cool at school, however, with teachers reporting that many are reluctant to remove their masks, even with encouragement from staff.",
+    [],
+  );
+
+  var doc = useMemo(() => nlp.readDoc(text), []);
+
+  var sentences = useMemo(() => doc.sentences().out(its.value), []);
+  var sentencesWeights = useMemo(
+    () => doc.out(its.sentenceWiseImportance).map((e: any) => e.importance),
+    [],
+  );
+
+  const [highlightPercentage, setHighlightPercentage] = useState<number>(20);
+  const processText = useCallback((): JSX.Element => {
+    if (new Date().getTime() - lastDate.getTime() < 1000) {
+      return lastData;
+    }
+
+    try {
+      console.log("processing");
+      const cutoff = quantile(sentencesWeights, 1 - highlightPercentage / 100); // 하이라이팅 기준 설정
+
+      lastDate = new Date();
+      lastData = sentences.map((sentence, index) => {
+        const importance = sentencesWeights[index];
+        const shouldHighlight = importance >= cutoff && highlightPercentage > 0;
+        const backgroundColor = shouldHighlight
+          ? "rgba(151, 78, 175, " + (importance - 0.3) + ")"
+          : "transparent";
+        const style = {
+          backgroundColor: backgroundColor,
+          color: "white",
+          fontWeight: "normal", // bold 제거
+          trainsition: "background-color 0.5s",
+        };
+
+        return (
+          <div key={index}>
+            <span style={style}>{sentence}</span>
+          </div>
+        );
+      });
+
+      return lastData;
+    } catch (e) {
+      console.error(e);
+      return lastData;
+    }
+  }, [highlightPercentage]);
 
   return (
     <div className="flex w-full max-h-[calc(100dvh-2.5rem)] overflow-hidden flex-1 flex-grow">
-
       {/* <iframe
         src="https://obtuse.kr"
         className="w-3/4 h-3/4 border-2 border-gray-300"
         title="Example Site"
         /> */}
       <div className="prose prose-basic dark:prose-invert overflow-y-scroll w-full !max-w-full flex-1 flex-grow">
-        <div id="result" ref={resultRef} className="prose prose-basic !max-w-full dark:prose-invert w-full"
-        ></div>
-        {article?.body && (
-          <div dangerouslySetInnerHTML={{ __html: article.body }} className="w-full"/>
-        )}
+        <div
+          id="result"
+          className="prose prose-basic !max-w-full dark:prose-invert w-full"
+        >
+          {processText()}
+        </div>
+        {/* {article?.body && (
+          <div
+            dangerouslySetInnerHTML={{ __html: article.body }}
+            className="w-full"
+          />
+        )} */}
       </div>
-      
-      <Chat setCutOff={setCutoff} cutOff={cutoff} />
+
+      <Chat setCutOff={setHighlightPercentage} cutOff={highlightPercentage} />
     </div>
   );
 }
