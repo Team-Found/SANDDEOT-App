ff49faefd3e38f0e06de2fe5888648c61fe3993b
jaehwan02 <jhrick0129@gmail.com>
2024-08-29 16:09:03 +0900

    Post.tsx css 쓸모없는거 삭제

--- src/renderer/src/components/Feed/Post/Post.tsx
+++ src/renderer/src/components/Feed/Post/Post.tsx
@@ -62,37 +62,30 @@ export default function Post(props: {
   }, [props.reRender, props.rssID]);
 
   return rss ? (
-    <div className="w-full self-stretch p-3.5 border-variable-collection-primaryBd border-[1px] rounded-lg justify-start items-start gap-6 inline-flex mb-4">
-      <div className="grow shrink basis-0 flex-col justify-center items-start gap-2 inline-flex">
-        <div className="self-stretch flex-col justify-start items-start flex">
-          <div className="w-96 justify-between items-center inline-flex">
+    <div className="w-full p-3.5 border-variable-collection-primaryBd border-[1px] rounded-lg gap-6 flex mb-4">
+      <div className="flex-col justify-center gap-2 flex">
+        <div className="flex-col flex">
+          <div className="w-96 justify-between items-center flex">
             <div className="justify-center items-center gap-1 flex">
               <div className="w-3.5 h-3.5 rounded-full justify-center items-center flex overflow-clip">
                 <img className="w-10 h-10" src={rss[0].RSSImageURL} />
               </div>
-              <div className="text-gray-200 text-xs font-medium leading-3">
-                {rss[0].RSSName}
-              </div>
-              <div className="text-stone-300 text-xs font-normal leading-3">
-                {timeState}
-              </div>
-            </div>
-            <div className="opacity-0 justify-start items-center gap-1 flex">
-              <div className="w-4 h-4 py-0.5 justify-center items-center flex" />
+              <div className="text-gray-200 text-xs">{rss[0].RSSName}</div>
+              <div className="text-stone-300 text-xs">{timeState}</div>
             </div>
           </div>
-          <div className="self-stretch justify-start items-center inline-flex">
-            <div className="grow shrink basis-0 flex-col justify-center items-start gap-2.5 inline-flex">
-              <div className="self-stretch flex-col justify-start items-start gap-0.5 flex">
-                <div className="self-stretch text-white text-base font-semibold leading-snug">
+          <div className="items-center flex">
+            <div className="flex-col justify-center gap-2.5 flex">
+              <div className="flex-col gap-0.5 flex">
+                <div className="text-white font-semibold leading-snug">
                   {props.title}
                 </div>
-                <div className="self-stretch text-neutral-400 text-xs font-normal leading-none whitespace-normal break-all line-clamp-3">
+                <div className="text-neutral-400 text-xs leading-none line-clamp-3">
                   {removeHTMLTags(props.description)}
                 </div>
               </div>
 
-              <div className="self-stretch justify-start items-start gap-1.5 inline-flex w-full">
+              <div className="gap-1.5 flex w-full">
                 {props.body && <Photo photos={extractionImg(props.body)} />}
               </div>
             </div>
