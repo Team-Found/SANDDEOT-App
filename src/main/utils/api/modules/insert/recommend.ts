149a3d357087f21392f372bafaabb5f3389dfc55
jaehwan02 <jhrick0129@gmail.com>
2024-09-02 23:49:07 +0900

    secret

--- src/main/utils/api/modules/insert/recommend.ts
+++ src/main/utils/api/modules/insert/recommend.ts
@@ -45,21 +45,158 @@ export default async function articleRecommend(
     return [
       {
         rssID: 33,
-        rssName: "실패",
+        rssName: "9TO5Mac",
         rssUrl: "https://feeds.feedburner.com/ndtvnews-world-news",
-        favicon:
-          "https://www.ndtv.com/common/header/images/ndtv_logo_black.gif",
+        favicon: "알아서 잘",
         articleID: 577,
-        title:
-          "Hong Kong Pro-Democracy News Outlet, Editors Found Guilty Of Sedition",
+        title: "What to expect with Apple’s new iPad models coming this month",
         descript:
-          "Hong Kong pro-democracy news outlet Stand News and its two former chief editors were found guilty of sedition on Thursday, the first conviction of its kind since the city came under Chinese rule in 1997.",
+          "Apple is expected to unveil two new iPad models this fall, likely at its September 9th keynote. This will include a new entry-level 11th generation model, as well as the 7th generation of iPad mini. Both of these models have gone 2 years without an update, so we’re overdue for a refresh.",
         date: 1724900370,
-        thumbnail:
-          "https://c.ndtvimg.com/2024-08/565lduf_hong-kong_625x300_29_August_24.jpeg",
+        thumbnail: "",
         imgList: [],
         content:
-          '<p>Hong Kong pro-democracy news outlet Stand News and its two former chief editors were found guilty of sedition on Thursday, the first conviction of its kind since the city came under Chinese rule in 1997.</p>\n\n<p>The verdict is part of a crackdown on free speech in the former British colony that has seen critics of China jailed or forced into exile, following huge pro-democracy protests in 2019.</p>\n\n<p>Editors Chung Pui-kuen, 54, and Patrick Lam, 36, are the first journalists to be convicted of sedition since Britain handed Hong Kong over to China in 1997, and the ruling drew resounding international condemnation.</p>\n\n<p>Chung and Lam were in charge of Stand News, a Chinese-language website that gained a massive following during the protests in 2019, before it was raided and shut down in December 2021.</p>\n\n<p>On Thursday, district court judge Kwok Wai-kin said the pair were guilty of "conspiracy to publish and reproduce seditious publications". The parent company of Stand News, Best Pencil Limited, was also found guilty.</p>\n\n<p>"The line (Stand News) took was to support and promote Hong Kong local autonomy," according to a written judgement by Kwok.</p>\n\n<p>"It even became a tool to smear and vilify the Central Authorities (Beijing) and the (Hong Kong) SAR Government."</p>\n\n<p>Kwok also pointed to 11 articles published by Stand News that "caused potential detrimental consequences to national security" and had the intention of "seriously undermining" authorities in Beijing and Hong Kong.</p>\n\n<p>Lam was absent from court on Thursday due to illness.</p>\n\n<p>The judge granted the duo bail before their sentencing on September 26.</p>\n\n<h3><strong>Colonial-era law</strong></h3>\n\n<p>Chung and Lam were charged under a colonial-era law, which punishes sedition with a maximum jail term of two years.</p>\n\n<p>A recent security law enacted in March raised the jail term for sedition to seven years.</p>\n\n<p>In response to the verdict, the European Union called on Hong Kong to "stop prosecuting journalists".</p>\n\n<p>"The ruling risks inhibiting the pluralistic exchange of ideas and the free flow of information, both cornerstones of the economic success of Hong Kong," a EU spokesperson said.</p>\n\n<p>Speaking outside court, police chief superintendent Steve Li said the verdict "clearly illustrated the necessity and lawfulness" of the enforcement actions in 2021 against Stand News.</p>\n\n<p>Asked if the verdict would further affect press freedom, Li said it would "actually help".</p>\n\n<p>"It would let everyone know what kind of problems could risk breaching the law," he said.</p>\n\n<h3><strong>Not seditious</strong></h3>\n\n<p>But Beh Lih Yi from the Committee to Protect Journalists said the ruling showed that Hong Kong was "descending further into authoritarianism".</p>\n\n<p>"Journalism is not seditious," she said.</p>\n\n<p>Sarah Brooks, Amnesty International\'s China Director, called the verdict "one more nail in the coffin for press freedom in Hong Kong".</p>\n\n<p>Hong Kong has seen its standing in global press freedom rankings plummet in recent years.</p>\n\n<p>Chung had testified that the outlet was a platform for free speech and defended his decisions to publish articles critical of the government.</p>\n\n<p>But prosecutors accused them of bringing "hatred or contempt" to the Chinese and Hong Kong governments.</p>\n\n<p>More than 100 people, including supporters and media professionals, queued up for spots in the public gallery on Thursday morning.</p>\n\n<p>The verdict was also attended by representatives from various consulates -- including the United States, Britain, France, European Union, and Australia.</p><p><i>(Except for the headline, this story has not been edited by NDTV staff and is published from a syndicated feed.)</i></p>',
+          '<p><img src="https://i0.wp.com/9to5mac.com/wp-content/uploads/sites/6/2024/08/iPad-Air-5-M1-deals.webp?w=1500&quality=82&strip=all&ssl=1" /></p>',
+        articleUrl:
+          "https://www.ndtv.com/world-news/hong-kong-outlet-stand-news-and-editors-convicted-of-sedition-judge-rules-6445341",
+      },
+      {
+        rssID: 33,
+        rssName: "9TO5Mac",
+        rssUrl: "https://feeds.feedburner.com/ndtvnews-world-news",
+        favicon: "알아서 잘",
+        articleID: 577,
+        title: "This iOS 18 feature needs to be copied by YouTube and Netflix",
+        descript:
+          "When you’re going to watch a video on your iPhone, what’s the app you turn to? My guess is, in most cases, it’s YouTube. How about on your Apple TV? Probably Netflix. But in iOS 18 and tvOS 18, Apple has added a key subtitles feature to its own TV app that I hope inspires YouTube and Netflix to copy it ASAP.",
+        date: 1724900370,
+        thumbnail: "",
+        imgList: [],
+        content:
+          '<p><img src="https://i0.wp.com/9to5mac.com/wp-content/uploads/sites/6/2024/06/Screenshot-2024-06-10-at-2.06.55%E2%80%AFPM.jpg?w=1500&quality=82&strip=all&ssl=1" /></p>',
+        articleUrl:
+          "https://www.ndtv.com/world-news/hong-kong-outlet-stand-news-and-editors-convicted-of-sedition-judge-rules-6445341",
+      },
+      {
+        rssID: 33,
+        rssName: "9TO5Mac",
+        rssUrl: "https://feeds.feedburner.com/ndtvnews-world-news",
+        favicon: "알아서 잘",
+        articleID: 577,
+        title: "These 10 Apple products will likely be discontinued next week",
+        descript:
+          "Apple’s annual iPhone event is being held next week on September 9th, where we’ll see iPhone 16, new AirPods, some new Apple Watch models, and potentially more. However, the introduction of new products also means saying goodbye to older ones. These are some Apple products you probably won’t be able to buy from the Apple Store after that keynote.",
+        date: 1724900370,
+        thumbnail: "",
+        imgList: [],
+        content:
+          '<p><img src="https://i0.wp.com/9to5mac.com/wp-content/uploads/sites/6/2024/07/apple-device-lineup.jpg?w=1500&quality=82&strip=all&ssl=1" /></p>',
+        articleUrl:
+          "https://www.ndtv.com/world-news/hong-kong-outlet-stand-news-and-editors-convicted-of-sedition-judge-rules-6445341",
+      },
+      {
+        rssID: 33,
+        rssName: "9TO5Mac",
+        rssUrl: "https://feeds.feedburner.com/ndtvnews-world-news",
+        favicon: "알아서 잘",
+        articleID: 577,
+        title:
+          "iPhone 17 Air: How ultra-thin will it be? These hints are telling",
+        descript:
+          "Apple has a lot in store for September’s iPhone 16 Pro and iPhone 16. Next fall, however, will bring bigger changes thanks to a new iPhone 17 Air joining the lineup. The 17 Air’s main draw will be a radical, ultra-thin new design. But how thin can it really be? Here’s what to expect based on hints from current products.",
+        date: 1724900370,
+        thumbnail: "",
+        imgList: [],
+        content:
+          '<p><img src="https://i0.wp.com/9to5mac.com/wp-content/uploads/sites/6/2024/08/iphone-17-air.jpg?w=1500&quality=82&strip=all&ssl=1" /></p>',
+        articleUrl:
+          "https://www.ndtv.com/world-news/hong-kong-outlet-stand-news-and-editors-convicted-of-sedition-judge-rules-6445341",
+      },
+      {
+        rssID: 33,
+        rssName: "The Github Blog",
+        rssUrl: "https://feeds.feedburner.com/ndtvnews-world-news",
+        favicon: "알아서 잘",
+        articleID: 577,
+        title:
+          "2024 is the biggest global election year in history. What’s at stake for developers?",
+        descript:
+          "GitHub is considering what is at stake for our users and platform, how we can take responsible action to support free and fair elections, and how developers contribute to resilient democratic processes.",
+        date: 1724900370,
+        thumbnail: "",
+        imgList: [],
+        content:
+          '<p><img src="https://github.blog/wp-content/uploads/2023/10/Collaboration-DarkMode-2.png?w=1200" /></p>',
+        articleUrl:
+          "https://www.ndtv.com/world-news/hong-kong-outlet-stand-news-and-editors-convicted-of-sedition-judge-rules-6445341",
+      },
+      {
+        rssID: 33,
+        rssName: "The Github Blog",
+        rssUrl: "https://feeds.feedburner.com/ndtvnews-world-news",
+        favicon: "알아서 잘",
+        articleID: 577,
+        title:
+          "Research: quantifying GitHub Copilot’s impact on developer productivity and happiness",
+        descript:
+          "When the GitHub Copilot Technical Preview launched just over one year ago, we wanted to know one thing: Is this tool helping developers? The GitHub Next team conducted research using a combination of surveys and experiments, which led us to expected and unexpected answers.",
+        date: 1724900370,
+        thumbnail: "",
+        imgList: [],
+        content:
+          '<p><img src="https://github.blog/wp-content/uploads/2022/06/Copilot.jpeg?w=1200" /></p>',
+        articleUrl:
+          "https://www.ndtv.com/world-news/hong-kong-outlet-stand-news-and-editors-convicted-of-sedition-judge-rules-6445341",
+      },
+      {
+        rssID: 33,
+        rssName: "The Github Blog",
+        rssUrl: "https://feeds.feedburner.com/ndtvnews-world-news",
+        favicon: "알아서 잘",
+        articleID: 577,
+        title:
+          "Automating open source: How Ersilia distributes AI models to advance global health equity",
+        descript:
+          "Discover how the Ersilia Open Source Initiative accelerates drug discovery by using GitHub Actions to disseminate AI/ML models.",
+        date: 1724900370,
+        thumbnail: "",
+        imgList: [],
+        content:
+          '<p><img src="https://github.blog/wp-content/uploads/2024/05/Collaboration-LightMode-2-1.png?w=1200" /></p>',
+        articleUrl:
+          "https://www.ndtv.com/world-news/hong-kong-outlet-stand-news-and-editors-convicted-of-sedition-judge-rules-6445341",
+      },
+      {
+        rssID: 33,
+        rssName: "The Github Blog",
+        rssUrl: "https://feeds.feedburner.com/ndtvnews-world-news",
+        favicon: "알아서 잘",
+        articleID: 577,
+        title: "How to level up your Git game with GitHub CLI",
+        descript:
+          "Using Git in the CLI can improve your development speed and power. Here are our top eight commands for using GitHub via your command line.",
+        date: 1724900370,
+        thumbnail: "",
+        imgList: [],
+        content:
+          '<p><img src="https://github.blog/wp-content/uploads/2024/08/github-cli-header.png?w=1200" /></p>',
+        articleUrl:
+          "https://www.ndtv.com/world-news/hong-kong-outlet-stand-news-and-editors-convicted-of-sedition-judge-rules-6445341",
+      },
+      {
+        rssID: 33,
+        rssName: "The Github Blog",
+        rssUrl: "https://feeds.feedburner.com/ndtvnews-world-news",
+        favicon: "알아서 잘",
+        articleID: 577,
+        title: "How we improved availability through iterative simplification",
+        descript:
+          "Solving and staying ahead of problems when scaling up a system of GitHub’s size is a delicate process. Here’s a look at some of the tools in GitHub’s toolbox, and how we’ve used them to solve problems.",
+        date: 1724900370,
+        thumbnail: "",
+        imgList: [],
+        content:
+          '<p><img src="https://github.blog/wp-content/uploads/2023/12/Productivity-DarkMode-2-1.png?w=1200" /></p>',
         articleUrl:
           "https://www.ndtv.com/world-news/hong-kong-outlet-stand-news-and-editors-convicted-of-sedition-judge-rules-6445341",
       },
